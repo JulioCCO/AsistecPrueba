1f9777886cc8ca4ea157397b57130ca275305f8b
Julio Cesar <julio.cesar.castro.ortiz93@gmail.com>
2023-10-03 13:24:39 -0600

    schedule prueba de que cree el apk

--- src/screens/HorarioScreen.js
+++ src/screens/HorarioScreen.js
@@ -1,11 +1,45 @@
-import {View, Text} from 'react-native';
+import { View, StyleSheet, Text } from 'react-native';
 import React from 'react';
+import { Agenda } from 'react-native-calendars';
 
-const HorarioScreen= () => {
+const HorarioScreen = () => {
+
+    const eventos = {
+        '2023-09-12': [{ name: 'Reunión de trabajo' }],
+        '2023-09-15': [{ name: 'Entrevista de trabajo' }],
+        '2023-09-18': [{ name: 'Cita médica' }],
+    };
+
+    const renderEvento = (evento) => {
+        return (
+          <View style={styles.eventoContainer}>
+            <Text>{evento.name}</Text>
+          </View>
+        );
+      };
+
     return (
-        <View>
-            <Text>Horario</Text>
-        </View>
+        <View style={{ flex: 1 }}>
+        <Agenda
+          items={eventos}
+          renderItem={(item) => renderEvento(item)}
+          // Personaliza más la apariencia de la agenda según tus necesidades
+          // Consulta la documentación para opciones adicionales
+        />
+      </View>
     );
 }
+
+const styles = StyleSheet.create({
+    eventoContainer: {
+      backgroundColor: 'white',
+      borderRadius: 5,
+      padding: 10,
+      marginRight: 10,
+      marginTop: 17,
+    },
+    // Agrega más estilos según sea necesario
+  });
+  
 export default HorarioScreen;
+
